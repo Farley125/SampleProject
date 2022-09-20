e773d664e7ef823e6f497111641c2e4b99e7e8ba
BT_4N2_03 <email@email>
2022-09-20 11:43:00 -0400

    Added a print statement which displays my favorite movie.

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,6 @@
 public class Sample {
     public static void main(String[] args){
         System.out.println("Matthew Farley");
-        System.out.println("My favorite movie is Spider-man Far From Home");
+        System.out.println("My favorite movie is Spider-man Far From Home.");
     }
 }
