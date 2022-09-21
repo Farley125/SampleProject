515a17d782e35d480d4bb98b1a3c58a82cdf0f1e
Matthew <mfarley3573@bths.edu>
2022-09-21 00:43:07 -0400

    I added a new print statement which prints what my favorite food is.

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args){
         System.out.println("Matthew Farley");
         System.out.println("My favorite movie is Spider-man Far From Home.");
+        System.out.println("My favorite food is buffalo wings.");
     }
 }
